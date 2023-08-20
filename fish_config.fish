834d487a24c478c6f12d15944ebc03f9a49fee1c
dave caruso <me@paperdave.net>
2023-08-19 21:10:59 -0700

    add more things no one asked for

--- fish_config.fish
+++ fish_config.fish
@@ -3,12 +3,12 @@ source ~/Code/config/fish_prompt.fish
 set fish_greeting
 
 function _wrap_with_tone
-    set absolutePath (which $scriptName)
+    set absolutePath (which $argv[1])
     alias $argv[1] "tone $absolutePath"
 end
 function _wrap_with_custom
-    set absolutePath (which $scriptName)
-    alias $argv[1] "$argv[2] $absolutePath"
+    set absolutePath (which $argv[1])
+    alias $argv[1] $argv[2]" $absolutePath"
 end
 
-_wrap_with_tone make
+_wrap_with_custom make _make
